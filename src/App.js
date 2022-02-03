849c064883233afff063cb5254aa5bbb127a4b02
jtamyrc <jason.tam@yource.cc>
2022-02-03 09:24:52 +0000

    update title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    This is a github action demo
+                    This is a
                 </a>
             </header>
         </div>
