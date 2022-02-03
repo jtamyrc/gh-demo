7316b1c007adece12133961651fe4f83b33a56b9
jtamyrc <jason.tam@yource.cc>
2022-02-03 09:16:52 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# A demo
+
+![example workflow](https://github.com/jtamyrc/gh-demo/actions/workflows/main.yml/badge.svg)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>A testing Project</p>
+                <a
+                    className="App-link"
+                    href="https://github.com/jtamyrc/gh-demo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    This is a github action demo
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
