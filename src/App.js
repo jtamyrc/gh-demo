2df76222ad166d7d7509f0bd4ced5ec19d3a2fb5
jtamyrc <jason.tam@yource.cc>
2022-02-08 10:59:59 +0000

    add demo word

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    This is a for Github Action
+                    This is a demo for Github Action
                 </a>
             </header>
         </div>
